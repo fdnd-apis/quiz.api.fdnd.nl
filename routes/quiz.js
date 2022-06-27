589588cf535f75b22d1ff49cb82c3cc5cc3cb0c7
Justus Sturkenboom <justus@ju5tu5.nl>
2022-06-27 18:20:38 +0200

    Updated post route
    This route had the 'create' function call as well..

--- routes/quiz.js
+++ routes/quiz.js
@@ -8,7 +8,7 @@ module.exports = express
   .post('/', async (req, res, next) => {
     try {
       console.log('Got: ', req.body)
-      res.json(await Quiz.create(new Quiz(req.body)))
+      res.json(await Quiz.post(new Quiz(req.body)))
     } catch (err) {
       console.error('Error while adding: ', err.message)
       next(err)
